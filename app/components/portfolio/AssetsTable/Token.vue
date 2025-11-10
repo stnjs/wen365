5521988ecba254c1a9d48d6886f6c17bff4eaa2f
mtp721 <nong_tung335@yahoo.de>
2025-11-10 15:52:05 +0100

    display assets with token avatar

---
+++ app/components/portfolio/AssetsTable/Token.vue
@@ -0,0 +1,15 @@
+<template>
+  <UUser
+    :name="props.tokenMetadata.name"
+    :avatar="{
+      src: props.tokenMetadata.logo,
+      icon: 'i-lucide-image',
+    }"
+    :description="props.tokenMetadata.symbol"
+  />
+</template>
+<script setup lang="ts">
+const props = defineProps<{
+  tokenMetadata: TokenMetadataDto;
+}>();
+</script>
