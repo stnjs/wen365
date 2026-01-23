e8a58b1f1c4c19e10cfc92e22fa315600abbf8b9
stnjs <57325418+stnjs@users.noreply.github.com>
2026-01-23 20:50:50 +0100

    feat: setup supabase

--- server/constants/blacklistedTokens.ts
+++ server/constants/blacklistedTokens.ts
@@ -5,4 +5,8 @@ export const BLACKLISTED_TOKENS: BlacklistedToken[] = [
     address: "0x0b91b07beb67333225a5ba0259d55aee10e3a578",
     symbol: "MNEP",
   },
+  {
+    network: "base-mainnet",
+    address: "0x7e3bc46884146a93e10f1ff4ad25355b4aca8d7a",
+  },
 ] as const;
